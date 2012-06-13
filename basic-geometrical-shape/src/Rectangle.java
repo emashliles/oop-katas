91e968cbbc0e81ebb08d7764434ecb2f0c5119ce
Caroline Kok <cgakok@gmail.com>
2012-06-13 12:36:56 +0100

    TA + CK: created basic shape class

---
+++ basic-geometrical-shape/src/Rectangle.java
@@ -0,0 +1,13 @@
+public class Rectangle {
+    private final double width;
+    private final double height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public double calculateArea() {
+        return width*height;
+    }
+}
