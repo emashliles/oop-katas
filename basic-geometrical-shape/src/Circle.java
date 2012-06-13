a79e4a8cb5babf327d01466693dab97e6f57b556
tajimobi <tosinajimobi@gmail.com>
2012-06-13 12:36:44 +0100

    TA + CK: created basic shape classes

---
+++ basic-geometrical-shape/src/Circle.java
@@ -0,0 +1,15 @@
+public class Circle {
+    private double radius;
+
+    public Circle(double radius) {
+
+        this.radius = radius;
+    }
+
+    public double calculateArea() {
+        return radius * radius * Math.PI;
+    }
+
+}
+
+
---
+++ basic-geometrical-shape/src/Triangle.java
@@ -0,0 +1,16 @@
+public class Triangle {
+
+    private double width;
+    private double height;
+
+    public Triangle(double width, double height) {
+
+        this.width = width;
+        this.height = height;
+    }
+
+    public double calculateArea(){
+        return width * height/2;
+    }
+
+}
