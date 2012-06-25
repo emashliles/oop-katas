4c0c32290460c6ada81df8c25233b4bab767d177
emashliles <emashliles@gmail.com>
2012-06-25 10:37:55 +0100

    EA and Caroline Final commit

---
+++ basic-code-smells/src/inheritance/vs/composition/BoardGameInfo.java
@@ -0,0 +1,7 @@
+package inheritance.vs.composition;
+
+public class BoardGameInfo {
+    public BoardGameInfo(String name, String s) {
+        //To change body of created methods use File | Settings | File Templates.
+    }
+}
---
+++ basic-code-smells/test/inheritance/vs/composition/BoardGameInfoTest.java
@@ -0,0 +1,13 @@
+package inheritance.vs.composition;
+
+import org.junit.Test;
+
+public class BoardGameInfoTest {
+
+    @Test
+    public void testlendForTwoMonths() throws Exception {
+
+        BoardGameInfo boardGameInfo = new BoardGameInfo("Hungry Hippos", "6");
+        boardGameInfo.
+    }
+}
